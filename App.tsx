70b2d760e7590b267bd25043f33d0032094ad9f1
Ayush Gupta <ayush.gupta@razorpay.com>
2024-01-11 12:57:36 +0530

    build: add rngh

--- App.tsx
+++ App.tsx
@@ -3,44 +3,25 @@ import Animated, {
   withTiming,
   useAnimatedStyle,
   Easing,
-} from "react-native-reanimated";
-import { View, Button } from "react-native";
+} from 'react-native-reanimated';
+import { View, Text } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
-
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
-
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
-    };
-  });
-
+const CatchUp = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
         }}
-      />
-    </View>
+      >
+        <Text>Yo</Text>
+      </View>
+    </GestureHandlerRootView>
   );
-}
+};
+
+export default CatchUp;
