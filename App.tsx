10bb2dbe35bd1b0f3a1d5edbf35e20b479f14ab3
Ayush Gupta <ayush.gupta@razorpay.com>
2024-01-11 13:04:43 +0530

    feat: add minimal pan gesture

--- App.tsx
+++ App.tsx
@@ -2,12 +2,42 @@ import Animated, {
   useSharedValue,
   withTiming,
   useAnimatedStyle,
-  Easing,
 } from 'react-native-reanimated';
-import { View, Text } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { View, StyleSheet } from 'react-native';
+import {
+  GestureHandlerRootView,
+  GestureDetector,
+  Gesture,
+} from 'react-native-gesture-handler';
+
+const END_POSITION = 200;
 
 const CatchUp = () => {
+  const onLeft = useSharedValue(true);
+  const position = useSharedValue(0);
+
+  const panGesture = Gesture.Pan()
+    .onUpdate((e) => {
+      if (onLeft.value) {
+        position.value = e.translationX;
+      } else {
+        position.value = END_POSITION + e.translationX;
+      }
+    })
+    .onEnd((e) => {
+      if (position.value > END_POSITION / 2) {
+        position.value = withTiming(END_POSITION, { duration: 100 });
+        onLeft.value = false;
+      } else {
+        position.value = withTiming(0, { duration: 100 });
+        onLeft.value = true;
+      }
+    });
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: position.value }],
+  }));
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View
@@ -18,10 +48,22 @@ const CatchUp = () => {
           flexDirection: 'column',
         }}
       >
-        <Text>Yo</Text>
+        <GestureDetector gesture={panGesture}>
+          <Animated.View style={[styles.box, animatedStyle]}></Animated.View>
+        </GestureDetector>
       </View>
     </GestureHandlerRootView>
   );
 };
 
 export default CatchUp;
+
+const styles = StyleSheet.create({
+  box: {
+    height: 120,
+    width: 120,
+    backgroundColor: '#b58df1',
+    borderRadius: 20,
+    marginBottom: 30,
+  },
+});
